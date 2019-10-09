7fb55ef7e339e10ba77700b22a65259789765645
Lorenzo L. Romero <lorenzolrom@gmail.com>
2019-10-09 16:15:04 -0400

    Added (unused) class to match version numbers with InfoScape.

---
+++ src/Version.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * LLR Technologies & Associated Services
+ * Information Systems Development
+ *
+ * INS WEBNOC API
+ *
+ * User: lromero
+ * Date: 10/09/2019
+ * Time: 4:14 PM
+ */
+
+
+abstract class Version
+{
+    const SOFTWARE_TITLE = "Mercury InfoCentral";
+    const CURRENT_VERSION = "1.1.7";
+}
